dd9ee7f65f7d30eae6e361ed064065765ea14e81
Sentarion <mbalabox@gmail.com>
2020-09-27 03:08:04 +0200

    DbService

--- src/main/java/com/crud/tasks/mapper/TaskMapper.java
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -2,10 +2,11 @@ package com.crud.tasks.mapper;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-
+import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class TaskMapper {
     public Task mapToTask(final TaskDto taskDto) {
         return new Task(
