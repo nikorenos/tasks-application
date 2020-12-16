2d8aad80c0d37df20cc9c5bf81f4ca12d6a24b6c
Sentarion <mbalabox@gmail.com>
2020-12-16 13:22:47 +0100

    limit swagger to com.crud.tasks.controller

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -22,7 +22,7 @@ public class CoreConfiguration {
     @Bean public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
             .paths(PathSelectors.any())
             .build();
     }
