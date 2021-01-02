a0902a08a651d1f837af2a96ba08ba8691c6b14d
Sentarion <mbalabox@gmail.com>
2021-01-02 15:47:59 +0100

    facade course code WIP

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -1,14 +1,13 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
-import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -31,8 +30,8 @@ public class TrelloService {
         return trelloClient.getTrelloBoards();
     }
 
-    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
-        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+    public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
                 "New card: " + card.getName() + " has been created on your Trello account")));
         return newCard;
--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -1,24 +1,40 @@
 package com.crud.tasks.trello.facade;
 
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloList;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
+import java.util.stream.Collectors;
 
 public class TrelloFacade {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloFacade.class);
     @Autowired
     private TrelloService trelloService;
     @Autowired
     private TrelloMapper trelloMapper;
 
-    public List<TrelloBoard> fetchTrelloBoards() {
-        return trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        List<TrelloBoard> trelloBoards = trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
+        LOGGER.info("Starting filtering boards...");
+        List<TrelloBoard> filteredBoards = trelloBoards.stream()
+                .filter(trelloBoard -> !trelloBoard.getName().equalsIgnoreCase("test"))
+                .collect(Collectors.toList());
+        LOGGER.info("Boards have been filtered. Current list size: " + filteredBoards.size());
+        return trelloMapper.mapToBoardsDto(filteredBoards);
+    }
+
+    public CreatedTrelloCardDto createCard(final TrelloCardDto trelloCardDto) {
+        TrelloCard trelloCard = trelloMapper.mapToCard(trelloCardDto);
+        if(trelloCard.getName().contains("test")) {
+            LOGGER.info("Someone is testing my application!");
+        } else {
+            LOGGER.info("Seems that my application is used in proper way!");
+        }
+        return trelloService.createTrelloCard(trelloMapper.mapToCardDto(trelloCard));
     }
 }
