1af03f5656955d5d4bdabbf06498cea90ca553df
Sentarion <mbalabox@gmail.com>
2021-01-05 16:33:40 +0100

    code coverage tests task WIP

--- src/test/java/com/crud/tasks/service/TrelloServiceTest.java
+++ src/test/java/com/crud/tasks/service/TrelloServiceTest.java
@@ -7,12 +7,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 
@@ -25,8 +29,11 @@ public class TrelloServiceTest {
     @Mock
     private TrelloClient trelloClient;
 
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
     @Mock
-    private SimpleEmailService emailService;
+    private JavaMailSender javaMailSender;
 
     @Test
     public void fetchTrelloBoards() {
@@ -50,17 +57,46 @@ public class TrelloServiceTest {
         AttachmentByTypeDto attachment = new AttachmentByTypeDto();
         BadgesDto badgesDto = new BadgesDto(4, attachment);
         CreatedTrelloCardDto cardDto = new CreatedTrelloCardDto("1","Card 1", "https://test.com", badgesDto);
-        Mail mail = new Mail("receiver", "mail", "test mail");
-        emailService.send(mail);
+        Mail mail = new Mail("test@test.com", "testCC@test.com", "Test", "Test Message");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        if (mail.getToCc() != null) {
+            mailMessage.setCc((mail.getToCc()));
+        }
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
 
         when(trelloClient.createNewCard(trelloCardDto)).thenReturn(cardDto);
 
         // When
-        //CreatedTrelloCardDto createdCard = trelloService.createTrelloCard(trelloCardDto);
+        simpleEmailService.send(mail);
+        CreatedTrelloCardDto createdCard = trelloService.createTrelloCard(trelloCardDto);
         // Then
-        /*assertEquals("1", createdCard.getId());
+        Mockito.verify(javaMailSender, times(1)).send(mailMessage);
+        assertEquals("1", createdCard.getId());
         assertEquals("Card 1", createdCard.getName());
         assertEquals("https://test.com", createdCard.getShortUrl());
-        assertEquals(4, createdCard.getBadges().getVotes());*/
+        assertEquals(4, createdCard.getBadges().getVotes());
+    }
+
+    @Test
+    public void shouldSendEmailWithCC() {
+        //Given
+        Mail mail = new Mail("test@test.com", "testCC@test.com", "Test", "Test Message");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        if (mail.getToCc() != null) {
+            mailMessage.setCc((mail.getToCc()));
+        }
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        Mockito.verify(javaMailSender, times(1)).send(mailMessage);
     }
 }
