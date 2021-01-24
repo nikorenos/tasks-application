b06d845cca15fca480781dc7e150be9031756534
Sentarion <mbalabox@gmail.com>
2021-01-24 21:40:34 +0100

    mail properties

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -9,4 +9,14 @@ import org.springframework.stereotype.Component;
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
+    @Value("${admin.name}")
+    private String adminName;
+    @Value("${info.company.name}")
+    private String infoCompanyName;
+    @Value("${info.company.goal}")
+    private String infoCompanyGoal;
+    @Value("${info.company.email}")
+    private String infoCompanyEmail;
+    @Value("${info.company.phone}")
+    private String infoCompanyPhone;
 }
